e0c1af0b491573233361edb86baf91023a27bb53
e-nikolov <emil.e.nikolov@gmail.com>
2023-11-02 07:41:43 +0100

    mpyc-web: better live stats

--- mpyc-web-demo/src/ts/app/format.ts
+++ mpyc-web-demo/src/ts/app/format.ts
@@ -6,6 +6,7 @@ ansi.enabled = true;
 
 declare module Colors {
     function peerID(text: string): string;
+    function grey50(text: string): string;
 }
 
-export const format: typeof ansi & typeof Colors = { ...ansi, peerID: ansi.redBright };
+export const format: typeof ansi & typeof Colors = { ...ansi, peerID: ansi.redBright, grey50: (s: string) => `\x1b[0m\x1b[38;5;244m${s}` };
