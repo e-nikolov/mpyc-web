88bd1747e50df937820df043285ce672867d7025
e-nikolov <emil.e.nikolov@gmail.com>
2023-12-11 14:17:50 +0100

    standalone demo: cleanup

--- mpyc-web-demo-standalone/vite.config.ts
+++ mpyc-web-demo-standalone/vite.config.ts
@@ -1,51 +1,8 @@
-import fs from 'fs';
-import git from 'git-rev-sync';
-import { glob } from 'glob';
 import { resolve } from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-// import 'vite/types/importMeta.d';
-// import PyodidePlugin from "@pyodide/webpack-plugin"
-const hexLoader = {
-  name: 'hex-loader',
-  transform(code, id) {
-    const [path, query] = id.split('?');
-    if (query != 'raw-hex')
-      return null;
-
-    const data = fs.readFileSync(path);
-
-    return data;
-  }
-};
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-
   return {
-    // optimizeDeps: {
-    //   auto: true,
-    //   exclude: [
-    //     'mpyc-web-core'
-    //   ],
-    // },
-    // lib: {
-    //   entry: path.resolve(__dirname, "src/main.ts"),
-    //   fileName: "main",
-    //   formats: ["es"],
-    // },
-    resolve: {
-      alias: {
-        // "@core": "@mpyc-web/core/dev",
-        // "@pyscript/core": "https://cdn.jsdelivr.net/npm/@pyscript/core@0.2.8/index.js",
-        // '@pyscript/core': './node_modules/@pyscript/core/index.js',
-      }
-    },
-    define: {
-      "__BUILD_INFO__": { version: env.npm_package_version, dirty: git.isDirty(), deployment: env.APP_DEPLOYMENT, timestamp: Date.now(), time: new Date().toLocaleString("en-IE", { hour12: false }), revision: git.short("../") },
-    },
     base: "./",
     build: {
       sourcemap: true,
@@ -54,35 +11,11 @@ export default defineConfig(({ mode }) => {
       chunkSizeWarningLimit: 1500,
       target: 'esnext',
       rollupOptions: {
-        // external: ['@pyscript/core'],
         input: [
           resolve(__dirname, 'index.html'),
-          ...glob.sync('bench/**/*.html').map((path) => resolve(__dirname, path)),
-          ...glob.sync('test/**/*.html').map((path) => resolve(__dirname, path)),
-          // resolve(__dirname, 'bench/serializers/index.html'),
-          // resolve(__dirname, 'bench/timeouts/index.html'),
-          // resolve(__dirname, 'bench/all/index.html'),
-          // resolve(__dirname, 'bench/index.html'),
         ]
       }
     },
-    cacheDir: ".vite",
-    plugins: [
-      // checker({ typescript: true }),
-
-
-      // hexLoader,
-
-      // tsconfigPaths(),
-      // externalize({ externals: ["@pyscript/core", "polyscript"] }),
-      // new PyodidePlugin(),
-      // topLevelAwait({
-      //   // The export name of top-level await promise for each chunk module
-      //   promiseExportName: "__tla",
-      //   // The function to generate import names of top-level await promise in each chunk module
-      //   promiseImportName: i => `__tla_${i}`
-      // }),
-    ],
     server: {
       host: "0.0.0.0",
       port: 4001,
@@ -99,19 +32,6 @@ export default defineConfig(({ mode }) => {
         'Cross-Origin-Resource-Policy': 'cross-origin',
         "Accept-Ranges": "bytes"
       },
-      watch: {
-
-      },
-      fs: {
-        // allow: [
-        //   // "mpyc-web/", // and your source files
-        //   "/**/*.*", // and your source files
-        //   "src", // and your source files
-        //   "./src/**/*.woff2", // and your source files
-        //   "/src/**/*.woff2", // and your source files
-        //   "../mpyc-web-py", // and your source files
-        // ],
-      }
     }
   }
 })
