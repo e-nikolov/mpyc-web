0af84e5be3bf8b8b6d37ad8bfc33113653fe8910
e-nikolov <emil.e.nikolov@gmail.com>
2023-12-12 20:13:07 +0100

    .

---
+++ mpyc-web-demo/bench/channels/index.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+export function main() {
+
+    let out = document.querySelector<HTMLDivElement>("#out")
+
+    let n = 11111
+    let ch = new MessageChannel()
+    let xx = 0
+
+    // function toString() {
+    //     return `${this.name}\t\t${Math.round(1000 * this.N / (this.duration + 1)).toLocaleString()} ops/sec`
+    // }
+
+    function toString(t) {
+        // return Math.round(t / 1000).toLocaleString()   
+        return Math.round(t).toLocaleString()
+    }
+
+    ch.port1.onmessage = (e) => {
+        xx += 1
+        if (xx >= n) {
+            let tre = performance.now() - ts
+
+            out.innerHTML += `done receiving, ${toString(tre / 1000)}, ${toString(1000 * n / tre)} <br>`
+        }
+    }
+
+    out.innerHTML += `start sending <br>`
+
+    let ts = performance.now()
+
+    for (let i = 0; i < n; i++) {
+        ch.port2.postMessage("")
+    }
+
+    let tse = performance.now() - ts
+
+
+    out.innerHTML += `done sending, ${toString(tse / 1000)}, ${toString(1000 * n / tse)} <br>`
+}
---
+++ mpyc-web-demo/bench/channels2/index.ts
@@ -0,0 +1,53 @@
+'use strict';
+
+export function main() {
+    let out = document.querySelector("#out")
+    function log(msg, t, n) {
+        out.innerHTML += `${msg}, ${toString(t / 1000)}, ${toString(1000 * n / t)} <br>`
+    }
+    function toString(t) {
+        // return Math.round(t / 1000).toLocaleString()   
+        return (Math.round(100 * t) / 100).toLocaleString()
+    }
+
+    let n = 3_000_000
+    let ch = new MessageChannel()
+    let xx = 0
+
+    ch.port1.onmessage = (e) => {
+        xx += 1
+        if (xx > 1000000 - 10) {
+            log("done up to", 1000 * xx, 0)
+            log("n=", 1000 * n, 0)
+            ch.port2.postMessage(undefined)
+        }
+        if (xx % 100000 === 0) {
+            log("done up to", 1000 * xx, 0)
+            log("n=", 1000 * n, 0)
+            ch.port2.postMessage(undefined)
+
+
+        }
+        if (xx >= n) {
+            ch.port2.postMessage(undefined)
+            let tre = performance.now() - ts
+            // console.log("done receiving", toString(tre / 1000), toString(1000 * n / tre))
+            // self.postMessage(["proxy:js:display", `done receiving ${toString(tre / 1000)}, ${toString(1000 * n / tre)}\n`])
+            log("done receiving", tre, n)
+        }
+    }
+
+    console.log("start sending")
+    log("start sending", 1, 1)
+    // self.postMessage(["proxy:js:display", `start sending\n`])
+    let ts = performance.now()
+    for (let i = 0; i < n; i++) {
+        ch.port2.postMessage(undefined)
+    }
+
+    let tse = performance.now() - ts
+    // self.postMessage(["proxy:js:display", `done sending ${toString(tse / 1000)}, ${toString(1000 * n / tse)}\n`])
+    log("done sending", tse, n)
+    console.log("done sending", toString(tse / 1000), toString(1000 * n / tse))
+
+}
