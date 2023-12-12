d908257122da651220c2621efb3860e0647e0dc8
e-nikolov <emil.e.nikolov@gmail.com>
2023-12-12 21:46:41 +0100

    Updates

---
+++ assets/index-qmNVgDTe.js
@@ -0,0 +1,2 @@
+import"./modulepreload-polyfill-xwJNGKPl.js";/* empty css              */var e={version:"0.9.2",dirty:!0,timestamp:1702413980088,time:"12/12/2023, 21:46:20",revision:"a07c692"};document.title="MPyC Web - Test"+(e.deployment?` (${e.deployment})`:"");
+//# sourceMappingURL=index-qmNVgDTe.js.map
